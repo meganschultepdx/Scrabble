27898690120b6ed70e468b81cffd696df879aa00
megan schulte and Neil Bateman <neil@>
2019-04-25 15:55:11 -0700

    add logic to make final test pass

--- Scrabble.Tests/ModelTests/ScrabbleTests.cs
+++ Scrabble.Tests/ModelTests/ScrabbleTests.cs
@@ -10,31 +10,70 @@ namespace ScrabbleValuesTests
     public void GetScrabbleValue_GetsValue_int()
     {
       //Arrange
-      string word = "a";
-      ScrabbleValues newScrabbleValues = new ScrabbleValues();
+
 
       //Act
-      int wordValue = 1;
-      newScrabbleValues.SetScrabbleValues(wordValue);
-      int result = newScrabbleValues.GetWord();
+      var result = ScrabbleValues.ScrabbleScore("a");
 
       //Assert
-      Assert.AreEqual(wordValue, result);
+      Assert.AreEqual(1, result);
     }
+
     [TestMethod]
-    public void GetScrabbleValue_GetsValue_int()
+    public void GetScrabbleValueIgnoreCase()
     {
       //Arrange
-      string word = "a";
-      ScrabbleValues newScrabbleValues = new ScrabbleValues();
+
 
       //Act
-      int wordValue = 1;
-      newScrabbleValues.SetScrabbleValues(wordValue);
-      int result = newScrabbleValues.GetWord();
+      var result = ScrabbleValues.ScrabbleScore("A");
 
       //Assert
-      Assert.AreEqual(wordValue, result);
+      Assert.AreEqual(1, result);
     }
+
+    [TestMethod]
+    public void GetScrabbleCharArray()
+    {
+      //Arrange
+
+      //Act
+      var result = ScrabbleValues.ScrabbleScore("cat");
+
+      //Assert
+      Assert.AreEqual(5, result);
+    }
+    //
+    // [TestMethod]
+    // public void GetScrabbleValue_GetsValue_int()
+    // {
+    //   //Arrange
+    //   string word = "a";
+    //   ScrabbleValues newScrabbleValues = new ScrabbleValues();
+    //
+    //   //Act
+    //   int wordValue = 1;
+    //   newScrabbleValues.SetScrabbleValues(wordValue);
+    //   int result = newScrabbleValues.GetWord();
+    //
+    //   //Assert
+    //   Assert.AreEqual(wordValue, result);
+    // }
+    //
+    // [TestMethod]
+    // public void ScrabbleScore_WordToCharArray_array()
+    // {
+    //   //Arrange
+    //   string word = "cat";
+    //   ScrabbleScore newScrabbleScore = new ScrabbleScore();
+    //
+    //   //Act
+    //   string wordValue = {"C","A","T"};
+    //   newScrabbleScore.SetScrabbleScore(wordValue);
+    //   string result = newScrabbleScore.ScrabbleScore();
+    //
+    //   //Assert
+    //   Assert.AreEqual(wordValue, result);
+    // }
   }
 }
